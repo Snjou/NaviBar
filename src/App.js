1b0c2b792b9f3f4ef779bc94dd07ccf7c725a2a9
Lionel Cabarios <franciscolionel63@gmail.com>
2021-07-08 06:42:39 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="top-nav">
+      <a class="active" href="">HOME</a>
+      <a href="#">ABOUT</a>
+      <a href="#">SERVICE</a>
+      <a href="#">CONTACT</a>
+      <a class="right" href="">LOGIN</a>
+      
+    </div>
+  );
+}
+
+export default App;
